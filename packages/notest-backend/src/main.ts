16d93254c1e6b88b3fe69711316fb48e1acd7ba4
Piermatteo <piermatteocardinali@gmail.com>
2023-03-10 17:40:15 +0100

    fix drone#1

--- packages/notest-backend/src/main.ts
+++ packages/notest-backend/src/main.ts
@@ -1,45 +1,53 @@
 require('dotenv').config();
-import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
+import {NestFactory} from '@nestjs/core';
+import {FastifyAdapter} from '@nestjs/platform-fastify';
 import multipart from '@fastify/multipart';
-import { AppModule } from './app.module';
+import {AppModule} from './app.module';
 
 process.on('uncaughtException', function (err) {
-  console.log('main.ts - Uncaught exception: ', err);
-  console.log(err.stack);
+    console.log('main.ts - Uncaught exception: ', err);
+    console.log(err.stack);
 });
 
 declare const module: any;
 
 async function bootstrap() {
-  const adapter = new FastifyAdapter({ logger: true });
-  adapter.register(multipart, {
-    limits: {
-      fieldNameSize: 10000, // Max field name size in bytes
-      fieldSize: 1024 * 1024, // Max field value size in bytes
-      fields: 100, // Max number of non-file fields
-      fileSize: 100 * 1024 * 1024, // For multipart forms, the max file size in bytes
-      files: 100 // Max number of file fields
+    const adapter = new FastifyAdapter({logger: true});
+    adapter.register(multipart, {
+        limits: {
+            fieldNameSize: 10000, // Max field name size in bytes
+            fieldSize: 1024 * 1024, // Max field value size in bytes
+            fields: 100, // Max number of non-file fields
+            fileSize: 100 * 1024 * 1024, // For multipart forms, the max file size in bytes
+            files: 100 // Max number of file fields
+        }
+    });
+    const app = await NestFactory.create(AppModule, adapter);
+
+    const memoryData = process.memoryUsage();
+    const formatMemoryUsage = (data) => Math.round((data / 1024 / 1024) * 100) / 100;
+
+    function logger(req, res, next) {
+        console.log(
+            req.method,
+            req.pathname,
+            formatMemoryUsage(memoryData.heapUsed),
+            formatMemoryUsage(memoryData.heapTotal)
+        );
+        next();
     }
-  });
-  const app = await NestFactory.create(AppModule, adapter);
 
-  function logger(req, res, next) {
-    console.log(`Request...`, req.url);
-    next();
-  }
+    app.use(logger);
+    app.enableCors();
 
-  app.use(logger);
-  app.enableCors();
+    app.setGlobalPrefix('api');
 
-  app.setGlobalPrefix('api');
+    await app.listen(3005, '0.0.0.0');
 
-  await app.listen(3005, '0.0.0.0');
-
-  if (module.hot) {
-    module.hot.accept();
-    module.hot.dispose(() => app.close());
-  }
+    if (module.hot) {
+        module.hot.accept();
+        module.hot.dispose(() => app.close());
+    }
 }
 
 bootstrap();
