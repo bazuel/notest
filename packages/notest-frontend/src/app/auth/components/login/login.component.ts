0b75a605a99061a1932c0ab243de0f5d018b560f
Piermatteo <piermatteocardinali@gmail.com>
2023-03-21 15:13:44 +0100

    Fixed reset password login and update frontend

--- packages/notest-frontend/src/app/auth/components/login/login.component.ts
+++ packages/notest-frontend/src/app/auth/components/login/login.component.ts
@@ -14,6 +14,7 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   email = '';
   password = '';
+  passwordRepeated= '';
   showForgotPasswordPopup = false;
   showResetPasswordPopup = false;
   tokenForPasswordReset = '';
