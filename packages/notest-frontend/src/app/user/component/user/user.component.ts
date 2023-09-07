d0013daf1084837a64808ad605ec575fa405cfd5
Emanuele <emanuele.antimiani@gmail.com>
2023-09-07 10:30:52 +0200

    fixed save popup on user-form

--- packages/notest-frontend/src/app/user/component/user/user.component.ts
+++ packages/notest-frontend/src/app/user/component/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import {
   copyToClipboard,
   NTRole,
@@ -26,6 +26,7 @@ export class UserComponent implements OnInit {
   newPasswordRepeated?: string;
   passwordUpdated = false;
   currentPasswordMatch = true;
+  changingPassword = false;
 
   newPassword = '';
 
@@ -38,8 +39,15 @@ export class UserComponent implements OnInit {
 
   saving = false;
   newUser = false;
+
   error = false;
   errorMessage = '';
+
+  passwordError = false;
+  passwordErrorMessage = '';
+
+  success = false;
+  passwordSuccess = false;
 
   constructor(
     private location: Location,
@@ -56,10 +64,14 @@ export class UserComponent implements OnInit {
     if (userid) this.user = await this.userService.getUserById(userid);
     else this.user = { domains: [], roles: [] } as any;
     this.initialUser = structuredClone(this.user);
-    setInterval(() => {
-      this.showSaveButton = JSON.stringify(this.initialUser) !== JSON.stringify(this.user);
-      this.showChangePassword = this.canSavePassword();
-    }, 100);
+  }
+
+  @HostListener('click')
+  @HostListener('keyup')
+  controlChanges() {
+    this.showSaveButton =
+      this.error || JSON.stringify(this.initialUser) !== JSON.stringify(this.user);
+    this.showChangePassword = this.passwordError || this.canSavePassword();
   }
 
   async generateApiToken() {
@@ -72,53 +84,95 @@ export class UserComponent implements OnInit {
   }
 
   async changePassword() {
-    const token = this.tokenService.get();
-    this.saving = true;
+    this.changingPassword = true;
     this.currentPasswordMatch = await this.checkCurrentPassword();
     if (!this.currentPasswordMatch) {
-      this.error = true;
-      this.errorMessage = 'Current password is not correct.';
+      this.onChangePasswordError('Current password is not correct.');
       return;
     }
     if (this.newPassword !== this.newPasswordRepeated) {
-      this.error = true;
-      this.errorMessage = 'New passwords do not match.';
+      this.onChangePasswordError('New passwords do not match.');
       return;
     }
-    if (token && this.newPassword) {
-      const result = await this.authService.resetPassword(token, this.newPassword);
-      if (result) {
-        this.showChangePassword = false;
-        this.currentPassword = '';
-        this.newPassword = '';
-        this.newPasswordRepeated = '';
-        this.saving = false;
-      }
-    }
+    const success = await this.authService.resetPassword(
+      this.tokenService.get()!,
+      this.newPassword
+    );
+    if (success) this.onChangePasswordSuccess();
   }
 
   async saveUser() {
-    this.error = false;
-    if (!this.user.nt_userid && (!this.newPassword || !this.user.email)) {
-      this.error = true;
-      this.errorMessage = 'Email and password are required for new users.';
-      return;
-    }
-    if (this.newPassword && this.newPassword !== this.newPasswordRepeated) {
-      this.error = true;
-      this.errorMessage = 'New passwords do not match.';
-      return;
-    }
     this.saving = true;
+    if (!this.user.email) {
+      this.onSaveUserError('Email is required.');
+      return;
+    }
+    if (!this.user.nt_userid && !this.newPassword) {
+      this.onSaveUserError('A new password is required for new users.');
+      return;
+    }
+    if (!this.user.nt_userid && this.newPassword && this.newPassword !== this.newPasswordRepeated) {
+      this.onSaveUserError('Passwords do not match.');
+      return;
+    }
     if (this.newUser) this.user.password = this.newPassword;
     const user = await this.userService.save(this.user);
     if (user) {
       this.user = user;
       this.initialUser = structuredClone(this.user);
       this.saving = false;
-      this.showSaveButton = false;
-      if (this.newUser) this.location.replaceState(`/user/user-form?id=${this.user.nt_userid}`);
+      if (this.newUser) {
+        this.newUser = false;
+        this.newPassword = '';
+        this.newPasswordRepeated = '';
+        this.location.replaceState(`/user/user-form?id=${this.user.nt_userid}`);
+      }
+      this.onSaveUserSuccess();
     }
+  }
+
+  onSaveUserError(message: string) {
+    this.controlChanges();
+    this.saving = false;
+    this.error = true;
+    this.errorMessage = message;
+    setTimeout(() => {
+      this.error = false;
+      this.saving = false;
+    }, 3000);
+  }
+
+  onSaveUserSuccess() {
+    this.controlChanges();
+    this.saving = false;
+    this.success = true;
+    setTimeout(() => {
+      this.success = false;
+      this.saving = false;
+    }, 3000);
+  }
+
+  onChangePasswordSuccess() {
+    this.controlChanges();
+    this.currentPassword = '';
+    this.newPassword = '';
+    this.newPasswordRepeated = '';
+    this.passwordSuccess = true;
+    setTimeout(() => {
+      this.passwordSuccess = false;
+      this.changingPassword = false;
+      this.showSaveButton = false;
+    }, 3000);
+  }
+
+  onChangePasswordError(message: string) {
+    this.controlChanges();
+    this.passwordError = true;
+    this.passwordErrorMessage = message;
+    setTimeout(() => {
+      this.passwordError = false;
+      this.changingPassword = false;
+    }, 3000);
   }
 
   async checkCurrentPassword() {
@@ -129,11 +183,11 @@ export class UserComponent implements OnInit {
     toggle(value, this.user.roles);
   }
 
-  protected readonly NTRoleMap = NTRoleMap;
-  protected readonly NTRoleLabelsMap = NTRoleLabelsMap;
-
   private canSavePassword() {
     const newPasswords = !!this.newPassword && !!this.newPasswordRepeated;
     return !this.newUser && newPasswords;
   }
+  protected readonly NTRoleMap = NTRoleMap;
+
+  protected readonly NTRoleLabelsMap = NTRoleLabelsMap;
 }
